c1c8c2849cf88331e67c9b48726b204742abee02
Sam M <sammontalvojr@gmail.com>
2020-09-10 22:28:11 -0500

    removing routes that won't be needed

--- flatbook-backend/config/routes.rb
+++ flatbook-backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :notes, only:[:index,:show,:update,:create,:destroy]
+      resources :notes, only:[:update,:create,:destroy]
       post '/users', to: 'users#create'
       post '/auth', to: 'auth#create'
       get '/current_user', to: 'auth#show'
